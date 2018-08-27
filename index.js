7f7a7cb7cc356800b0a8e810f0f5fe4904381c69
zhujun.qiu <qzhujun@sina.com>
2018-08-27 18:21:55 +0800

    optimize

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-export default function(enter){
-	if(typeof window['define'] === "function"){
-	    window['define']([],function(){
-	        return enter;
-	    });
-	}else {
-	    enter('',null);
-	}
-}
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+			value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = function (enter, cr) {
+			if (typeof window['define'] === "function") {
+						window['define']([], function () {
+									return function (param, pr) {
+												if (pr.store && cr.reducers) {
+															pr.store.replaceReducer((0, _redux.combineReducers)(_extends({}, pr.reducers || {}, cr.reducers)));
+															cr.store = pr.store;
+												}
+												if (pr.actions && cr.actions) {
+															cr.actions = Object.assign(pr.actions, or.actions);
+												} else {
+															cr.actions = pr.actions || cr.actions || {};
+												}
+												enter(param, cr);
+									};
+						});
+			} else {
+						enter(null, cr || {});
+			}
+};
+
+var _redux = require('redux');
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+export default function(enter,cr){
+	if(typeof window['define'] === "function"){
+	    window['define']([],function(){
+	        return function(param,pr){
+	        	if(pr.store && cr.reducers ){
+	        		pr.store.replaceReducer(combineReducers({
+						...(pr.reducers||{}),
+						...cr.reducers
+					}));
+	        		cr.store = pr.store;
+				}
+				if(pr.actions && cr.actions){
+	        		cr.actions = Object.assign(pr.actions,or.actions);
+				}else{
+	        		cr.actions = pr.actions || cr.actions || {};
+				}
+				enter(param, cr);
+			}
+	    });
+	}else {
+	    enter(null,cr || {});
+	}
+}
